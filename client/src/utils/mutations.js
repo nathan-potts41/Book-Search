fece2339d3fb6e473f7dc91bd96922212c2f32de
nathan-potts41 <pottsandpans41@hotmail.com>
2021-03-15 14:01:43 -0500

    added queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,67 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookId: String!) {
+    saveBook(bookId: $id) {
+        User {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                [author]
+                bookId
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation saveBook($bookId: String!) {
+    saveBook(bookId: $id) {
+        User {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                [author]
+                bookId
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+  }
+`;
